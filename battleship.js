a8c753186beeb463fe099426f944d9049241beec
ZGoldman8 <ZGoldman@protonmail.com>
2018-11-12 12:16:26 -0800

    Player can now make guesses on grid

--- battleship.js
+++ battleship.js
@@ -23,34 +23,49 @@ var model = {
 		{ locations: ["24", "34", "44"], hits: ["", "", ""] },
 		{ locations: ["10", "11", "12"], hits: ["", "", ""] }],
 
-		fire: function(guess) {
-			for (var i = 0; i < this.numShips; i++) {
-				var ship = this.ships[i];
-				var index = ship.locations.indexOf(guess);
-				if (index >= 0) {
-					ship.hits[index] = "hit";
-					view.displayHit(guess);
-					view.displayMessage("HIT!");
-					if (this.isSunk(ship)) {
-						view.displayMessage("You sank my battleship!");
-						this.shipsSunk++;
-					}
-					return true;
+	fire: function(guess) {
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = this.ships[i];
+			var index = ship.locations.indexOf(guess);
+			if (index >= 0) {
+				ship.hits[index] = "hit";
+				view.displayHit(guess);
+				view.displayMessage("HIT!");
+				if (this.isSunk(ship)) {
+					view.displayMessage("You sank my battleship!");
+					this.shipsSunk++;
 				}
+				return true;
 			}
-			view.displayMiss(guess);
-			view.displayMessage("You missed.");
-			return false;
-		},
-		isSunk: function(ship) {
-			for (var i = 0; i < this.shipLength; i++) {
-				if (ship.hits[i] !== "hit") {
-					return false;
-				}
-			}
-			return true;
 		}
+		view.displayMiss(guess);
+		view.displayMessage("You missed.");
+		return false;
+	},
+	isSunk: function(ship) {
+		for (var i = 0; i < this.shipLength; i++) {
+			if (ship.hits[i] !== "hit") {
+				return false;
+			}
+		}
+		return true;
+	}
 };
+
+var controller = {
+	guesses: 0,
+
+	processGuess: function(guess) {
+		var location = parseGuess(guess);
+		if (location) {
+			this.guesses++;
+			var hit = model.fire(location);
+			if (hit && model.shipsSunk === model.numShips) {
+				view.displayMessage("You sank all my battleships in " + this.guesses + " guesses!");
+			}
+		}
+	}
+}
 
 function parseGuess(guess) {
 	var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
@@ -72,3 +87,27 @@ function parseGuess(guess) {
 		return null;
 	}
 }
+
+function init() {
+	var fireButton = document.getElementById("fireButton");
+	fireButton.onclick = handleFireButton;
+	var guessInput = document.getElementById("guessInput");
+	guessInput.onkeypress = handleKeyPress;
+}
+
+function handleFireButton() {
+	var guessInput = document.getElementById("guessInput");
+	var guess = guessInput.value;
+	controller.processGuess(guess);
+	guessInput.value = "";
+}
+
+function handleKeyPress(e) {
+	var fireButton = document.getElementById("fireButton");
+	if (e.keyCode === 13) {
+		fireButton.click();
+		return false;
+	} 
+}
+
+window.onload = init;
